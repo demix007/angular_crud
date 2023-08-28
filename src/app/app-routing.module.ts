05af3fb853cfd687bd7f40bbfe4b82baab406c9b
demix007 <ayodejiabidemi007@gmail.com>
2023-08-28 18:18:14 +0100

    ADD PROJECT FILES

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssociatelistingComponent } from './component/associatelisting/associatelisting.component';
+import { CustomerlistingComponent } from './component/customerlisting/customerlisting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AssociatelistingComponent},
+  {path:'customer',component:CustomerlistingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
